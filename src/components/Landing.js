b3aaf2f990d57310d8172b8a9290313637b4a38f
Jorge <djsmoothzouk@gmail.com>
2021-03-27 22:12:43 -0500

    lined up items on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,15 +7,15 @@ const Landing = () => {
     return (
         <>
         <div>
-          <Link to="/home">
-            <button className="button">Hello</button>
-          </Link>
         </div>
         <div className="container">
             <div className="logo">
                 <img className="logo" src="https://res.cloudinary.com/acurunner79/image/upload/v1616873759/website_logo_transparent_background_2_ahhr1g.png" alt="landing-logo"/>
             </div>
-            <div className="shadow"></div>
+            
+          <Link to="/home">
+            <button className="button"><h3>Enter Site</h3></button>
+          </Link>
         </div>
         </>
     )
