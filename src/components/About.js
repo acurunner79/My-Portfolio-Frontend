74df81081ac53366d202df0f1600d0f1dd388d7a
Jorge <djsmoothzouk@gmail.com>
2021-07-07 20:01:34 -0500

    styled contact card

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ValidationError, useForm } from '@formspree/react'
+import './about.css'
 
 
 
@@ -7,43 +8,43 @@ const About = () => {
 
    const [state, handleSubmit] = useForm("meqvklpb");
       if (state.succeeded) {
-        return <div><h1>Thank you!</h1></div>
+        return <div><h1>Thank you! </h1></div>
       }
       
     return(
-      <>
-         <div>
-            <p></p>
-         </div>
+      <div id="contact-card">
          <h1>Contact Me</h1>
+         <div>
+            <h4 id="contact-header">Thank you for stopping by! If you'd like to contact me regarding a project, or have any questions at all, please feel free to send me a message below.</h4>
+         </div>
             <form onSubmit={handleSubmit}>
                <ValidationError field="email" prefix="Email" errors={state.errors} />
-               <input
+               <input className="inputs"
                   type="name"
                   name="first name"
                   placeholder="First Name">
                </input>
                   <br/>
-               <input
+               <input className="inputs"
                   type="name"
                   name="last name"
                   placeholder="Last Name">
                </input>
                   <br/>
-               <input
+               <input className="inputs"
                   type="email"
                   name="email"
                   placeholder="Email Address">
                </input>
                   <br/>
-               <input
+               <textarea
                   id="message"
                   name="message">
-               </input>
+               </textarea>
                   <br/>
                <button className="card-button" type="submit" disabled={state.submitting}><h4>Submit</h4></button>
             </form>
-      </>
+      </div>
    )
 }
 
